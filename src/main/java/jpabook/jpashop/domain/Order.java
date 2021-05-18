12290b639725e5936db2f401df5a006d419d0ac6
wonjjong <wonjjong.dev@gmail.com>
2021-05-18 16:49:37 +0900

    Database domain coding

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -14,22 +14,41 @@ import java.util.List;
 @Setter
 public class Order {
 
-    @Id @GeneratedValue
-    @Column(name="order_id")
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name="member_id") //FK
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id") //FK
     private Member member;
 
-    @OneToMany(mappedBy="order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
-    @JoinColumn(name="delivery_id")
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
+
+    //==연관관계 편의 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+
 }
